db3a84219e500de26c1fdc51ef0ec41ade69c262
Alex Booker <booker@booker.codes>
2024-04-16 10:49:15 +0100

    Limit non-mod capabilities

--- app/api/ably/route.js
+++ app/api/ably/route.js
@@ -1,11 +1,14 @@
 import { currentUser } from '@clerk/nextjs';
 import { SignJWT } from "jose";
 
-export const createJwt = (clientId, apiKey, claim) => {
+export const createJwt = (clientId, apiKey, claim, capability) => {
   const [appId, signingKey] = apiKey.split(":", 2);
   const enc = new TextEncoder();
+
+  const x = JSON.stringify(capability)
+  console.log("x", x)
   const token = new SignJWT({
-    "x-ably-capability": `{"*":["*"]}`,
+    "x-ably-capability": x,
     "x-ably-clientId": clientId,
     "ably.channel.*": JSON.stringify(claim)
     // 'ably.limits.publish.perAttachment.maxRate.chat': 0.1,
@@ -20,10 +23,26 @@ export const createJwt = (clientId, apiKey, claim) => {
 export const GET = async () => {
   const user = await currentUser()
   // todo - put Clerk metadata into Ably claim
-  const role = 'swafggg'
-  console.log('role', role)
   const claim = user.publicMetadata
-  const tokenDetails = await createJwt(user.id, process.env.ABLY_SECRET_KEY, claim)
+
+  // Capabilities are a map from resources to a list of operations. 
+  // A resource of namespace:* will match any channel in the namespace namespace, including namespace:channel, and namespace:channel:other
+  // capabilities = subscribe, publish, presence, history, stats, publish-subscribe
+
+  let capability
+
+  if (claim.isMod) {
+    capability = {
+      '*': ['*']
+    }
+  } else  {
+    capability = {
+      'chat:general': ['subscribe', 'publish', 'presence'],
+      'chat:random': ['subscribe', 'publish', 'presence'],
+    }
+  }
+
+  const tokenDetails = await createJwt(user.id, process.env.ABLY_SECRET_KEY, claim, capability)
 
   return Response.json(tokenDetails)
 }
