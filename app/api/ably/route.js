0a041ff8989663c5eb52816710686f69d5c29a34
Alex Booker <booker@booker.codes>
2024-04-16 11:10:27 +0100

    Refactor route

--- app/api/ably/route.js
+++ app/api/ably/route.js
@@ -1,14 +1,12 @@
 import { currentUser } from '@clerk/nextjs';
-import { SignJWT } from "jose";
+import { SignJWT } from "jose"
 
-export const createJwt = (clientId, apiKey, claim, capability) => {
-  const [appId, signingKey] = apiKey.split(":", 2);
-  const enc = new TextEncoder();
+const createToken = (clientId, apiKey, claim, capability) => {
+  const [appId, signingKey] = apiKey.split(":", 2)
+  const enc = new TextEncoder()
 
-  const x = JSON.stringify(capability)
-  console.log("x", x)
   const token = new SignJWT({
-    "x-ably-capability": x,
+    "x-ably-capability": JSON.stringify(capability),
     "x-ably-clientId": clientId,
     "ably.channel.*": JSON.stringify(claim)
     // 'ably.limits.publish.perAttachment.maxRate.chat': 0.1,
@@ -16,34 +14,32 @@ export const createJwt = (clientId, apiKey, claim, capability) => {
     .setProtectedHeader({ kid: appId, alg: "HS256" })
     .setIssuedAt()
     .setExpirationTime("24h")
-    .sign(enc.encode(signingKey));
+    .sign(enc.encode(signingKey))
   return token
-};
+}
 
-export const GET = async () => {
-  const user = await currentUser()
-  // todo - put Clerk metadata into Ably claim
-  const claim = user.publicMetadata
-
-  // Capabilities are a map from resources to a list of operations. 
-  // A resource of namespace:* will match any channel in the namespace namespace, including namespace:channel, and namespace:channel:other
-  // capabilities = subscribe, publish, presence, history, stats, publish-subscribe
-
-  let capability
-
+const generateCapability = claim => {
   if (claim.isMod) {
-    capability = {
-      '*': ['*']
-    }
-  } else  {
-    capability = {
+    return { '*': ['*'] };
+  } else {
+    return {
       'chat:general': ['subscribe', 'publish', 'presence'],
       'chat:random': ['subscribe', 'publish', 'presence'],
       'chat:announcements': ['subscribe', 'presence']
     }
   }
+}
 
-  const tokenDetails = await createJwt(user.id, process.env.ABLY_SECRET_KEY, claim, capability)
+export const GET = async () => {
+  const user = await currentUser()
+  const userClaim = user.publicMetadata
+  const userCapability = generateCapability(userClaim)
 
-  return Response.json(tokenDetails)
+  const token = await createToken(
+    user.id,
+    process.env.ABLY_SECRET_KEY,
+    userClaim,
+    userCapability)
+
+  return Response.json(token)
 }
